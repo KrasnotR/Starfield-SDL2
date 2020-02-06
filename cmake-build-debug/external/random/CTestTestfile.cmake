60df559012c25a7bab978c21a129e8c65b0291aa
KrasnotR <krasnogorskotr@gmail.com>
2020-02-06 08:37:37 -0300

    removed stupid stuff

--- cmake-build-debug/external/random/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krasno/CLionProjects/Starfield-SDL2/external/random
-# Build directory: /home/krasno/CLionProjects/Starfield-SDL2/cmake-build-debug/external/random
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
